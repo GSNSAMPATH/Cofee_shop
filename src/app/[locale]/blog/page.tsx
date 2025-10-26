ef30c7bf2b12cdb368cbdae2ef6192ea97690486
GSNSAMPATH <sumalnadira123@gmail.com>
2025-10-26 12:32:07 +0530

    first commit

--- src/app/[locale]/blog/page.tsx
+++ src/app/[locale]/blog/page.tsx
@@ -1,7 +1,30 @@
+// import BlogPageClient from './BlogPageClient';
+// import { client } from '@/app/[locale]/lib/sanity.config';
+
+// // Server Component - async allowed
+// export default async function BlogPage({ params }: { params: { locale: string } }) {
+//   const { locale } = params;
+
+//   // Fetch blogs from Sanity
+//   const blogs = await client.fetch(`
+//     *[_type == "blog" && defined(slug.current)] | order(date desc){
+//       _id,
+//       title,
+//       excerpt,
+//       date,
+//       slug,
+//       mainImage
+//     }
+//   `);
+
+//   return <BlogPageClient blogs={blogs} locale={locale} />;
+// }
+
+
 import BlogPageClient from './BlogPageClient';
 import { client } from '@/app/[locale]/lib/sanity.config';
 
-// Server Component - async allowed
+// Server Component
 export default async function BlogPage({ params }: { params: { locale: string } }) {
   const { locale } = params;
 
@@ -19,3 +42,4 @@ export default async function BlogPage({ params }: { params: { locale: string } 
 
   return <BlogPageClient blogs={blogs} locale={locale} />;
 }
+
