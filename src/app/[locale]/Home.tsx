03b2fe90c044f0978840b0803205ec901482327f
GSNSAMPATH <sumalnadira123@gmail.com>
2025-10-27 23:07:35 +0530

    first commit

--- src/app/[locale]/Home.tsx
+++ src/app/[locale]/Home.tsx
@@ -91,7 +91,7 @@ export default function Home() {
             </motion.p>
             <a
               href={`/${locale}/offer`}
-              className="inline-flex items-center px-6 py-2 border border-red-500 rounded-full text-white font-medium 
+              className="inline-flex items-center px-6 py-2 border border-red-500 rounded-full text-white font-medium bg-red-600
                hover:bg-red-500 transition duration-300"
             >
               {t("viewOffer")}
